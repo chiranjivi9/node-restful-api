6037caaee501ee94cbde5a1b1aad3dea03a27023
Chiranjivi Jawale <cj@wunderfund.co>
2018-08-14 16:43:06 -0400

    add proper handling improved workflow

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
+
+const morgan = require('morgan');
 
 // create a path to the products folder in api
 const productRoute = require('./api/routes/products');
@@ -9,5 +11,25 @@ const orderRoute = require('./api/routes/orders');
 // api call to access data under products
 app.use('/products', productRoute);
 app.use('/orders', orderRoute);
+
+app.use((req,res,next)=>{
+
+    const error = new Error('Not Found');
+    error.status=400;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status||500);
+    res.json({
+        error:{
+            message : error.message
+
+        }
+    })
+
+
+});
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -9,3 +9,20 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
+
+
+//
+// <script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//               apiKey: "AIzaSyAaT_vgM4jjTxkXa2nx48kjVt1RxJ7BUdI",
+//               authDomain: "rest-api-5e8a6.firebaseapp.com",
+//               databaseURL: "https://rest-api-5e8a6.firebaseio.com",
+//               projectId: "rest-api-5e8a6",
+//               storageBucket: "",
+//               messagingSenderId: "135498282666"
+//             };
+//   firebase.initializeApp(config);
+// // </script>
+// const firebaseDB = firebase.database()
